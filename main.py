c5ecf571bf71ed0a0c9b2fce66d3f1395be5194d
jakobap <jakobpoerschmann@gmail.com>
2023-12-13 11:07:27 +0100

    type fix

--- main.py
+++ main.py
@@ -53,7 +53,8 @@ class DocPreview:
 
         st.write(f"[{doc_name}](%s)" % doc_link)
         st.image("PDF_file_icon.png", width=50)
-        st.button('delete', key=f'delete_{doc_name}', on_click=delete_file, args=(doc_name))
+        # print((doc_name))
+        st.button('delete', key=f'delete_{doc_name}', on_click=delete_file, args=[doc_name])
 
 def main(client_query:str, model_name: str) -> None:  
 
